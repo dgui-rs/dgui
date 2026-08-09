15cbe291b0699e31f87a1a32ce0cc34b71d1158b
sumeeth <sumeethveeramallu2001@gmail.com>
2026-08-10 03:31:48 +0530

    minor tweaks

--- src/layout/tessellation.rs
+++ src/layout/tessellation.rs
@@ -1,3 +1,13 @@
 pub struct Tessellate;
 
-impl Tessellate {}
+impl Tessellate {
+    pub fn rect() {}
+
+    pub fn circle() {}
+
+    pub fn ring() {}
+
+    pub fn path() {}
+
+    pub fn quad_uv() {}
+}
--- src/styles/types.rs
+++ src/styles/types.rs
@@ -2,3 +2,9 @@ pub enum FontStyle {
     Normal,
     Italic,
 }
+
+pub enum Dimension {
+    Auto,
+    Px(f32),
+    Percent(f32),
+}
